92f53eeb157073a5415fd856bd51eb9e0838f5ec
Laharih28 <laharih.28@gmail.com>
2025-02-04 18:59:16 +0530

    Update constants.groovy

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -1,13 +1,13 @@
 import groovy.transform.Field
 
-@Field String AWS_ACCOUNT_ID = '120569619769'
-@Field String AWS_DEFAULT_REGION = 'ap-south-1'
+@Field String AWS_ACCOUNT_ID = '727646512111'
+@Field String AWS_DEFAULT_REGION = 'us-east-1'
 @Field String IMAGE_REPO_NAME = 'sample-webapp'
-@Field String GIT_REPO_URL = 'https://github.com/rak134/devops-automation.git'
+@Field String GIT_REPO_URL = 'https://github.com/Laharih28/Jenkins-Shared-Lib.git'
 
 // Jenkins Credential IDs
 @Field String GIT_CREDENTIALS_ID = 'github-token'
-@Field String ECR_CREDENTIALS_ID = 'aws-credentials'
+@Field String ECR_CREDENTIALS_ID = '727646512111.dkr.ecr.us-east-1.amazonaws.com/devops12'
 
 def call() {
     return this
