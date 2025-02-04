92cd2d65472de6671ec1b70eff394b19c86ee4a4
Ubuntu <ubuntu@ip-172-31-80-163.ec2.internal>
2025-02-04 12:58:49 +0000

    first commit

---
+++ vars/constants.groovy
@@ -0,0 +1,14 @@
+import groovy.transform.Field
+
+@Field String AWS_ACCOUNT_ID = '120569619769'
+@Field String AWS_DEFAULT_REGION = 'ap-south-1'
+@Field String IMAGE_REPO_NAME = 'sample-webapp'
+@Field String GIT_REPO_URL = 'https://github.com/rak134/devops-automation.git'
+
+// Jenkins Credential IDs
+@Field String GIT_CREDENTIALS_ID = 'github-token'
+@Field String ECR_CREDENTIALS_ID = 'aws-credentials'
+
+def call() {
+    return this
+}
